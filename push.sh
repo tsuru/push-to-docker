42ad962ca9594eccf25514425408018b7b713e84
André Carvalho <andre.carvalho@corp.globo.com>
2016-10-07 14:44:52 -0300

    pushes only if  is set

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function tag_and_push {
-	if [ -n "$1" ]; then
-		echo "Pushing docker image to hub tagged as $1"
-		docker build -t $IMAGE_NAME:$1 .
-		docker push $IMAGE_NAME:$1
+	if [ -n "$1" ] && [ -n "$IMAGE_NAME" ]; then
+		echo "Pushing docker image to hub tagged as $IMAGE_NAME:$1"
+		#docker build -t $IMAGE_NAME:$1 .
+		#docker push $IMAGE_NAME:$1
 	fi
 }
 
