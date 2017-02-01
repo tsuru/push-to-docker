d0b59765a609c1216f2feff46a75993ca3c43f01
Paulo Sousa <paulo.sousa@corp.globo.com>
2017-02-01 15:46:58 -0200

    add script for build and tag platforms

---
+++ platform_build_tag.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function build_and_tag {
+	PLATFORM=$1
+	DOCKER_TAG=$2
+	if [ -z $DOCKER_TAG ]; then
+		DOCKER_TAG="latest"
+	fi
+	echo "Building platform and tagged as tsuru/${PLATFORM}:${DOCKER_TAG}"
+	pushd ${PLATFORM}
+	echo docker build -t tsuru/${PLATFORM}:${DOCKER_TAG} .
+	echo docker push tsuru/${PLATFORM}:${DOCKER_TAG}
+	popd
+}
+
+cat > ~/.dockercfg <<EOF
+{
+  "https://index.docker.io/v1/": {
+    "auth": "${HUB_AUTH}",
+    "email": "${HUB_EMAIL}"
+  }
+}
+EOF
+
+for PLATFORM in ${PLATFORMS}
+do
+	if [ -n "$TRAVIS_TAG" ] && [[ $TRAVIS_TAG =~ ${PLATFORM}-([0-9.]+) ]]; then
+		TAG=${BASH_REMATCH[1]}
+		build_and_tag "${PLATFORM}"
+		build_and_tag "${PLATFORM}" "${TAG}"
+	else
+		build_and_tag "${PLATFORM}"
+	fi
+done
